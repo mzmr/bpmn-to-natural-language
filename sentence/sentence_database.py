665dbb685e8b35252abfd280bfe8a67a2091321b
Maciej Znamirowski <maciek@znamirowski.pl>
2018-12-19 17:18:01 +0100

    Small fix. Uncommented the rest o start sentences.

--- sentence/sentence_database.py
+++ sentence/sentence_database.py
@@ -7,14 +7,14 @@ SentenceDef = namedtuple('SentenceDef', 'subject_infl subject_order predicate_in
 
 class SentenceDatabase:
     sentences_start = [
-        # SentenceDef(
-        #     InflectionParams('subst', 'sg', 'nom'), 1,
-        #     InflectionParams('inf', 'nom'), 2,
-        #     ('Na początku ', ' musi ', '.')),
-        # SentenceDef(
-        #     InflectionParams('subst', 'sg', 'acc', 'm1'), 2,
-        #     InflectionParams('ger', 'sg', 'nom', 'n', 'aff'), 1,
-        #     ('Pierwszą czynnością, jaką należy wykonać, jest ', ' przez ', '.')),
+        SentenceDef(
+            InflectionParams('subst', 'sg', 'nom'), 1,
+            InflectionParams('inf', 'perf'), 2,
+            ('Na początku ', ' musi ', '.')),
+        SentenceDef(
+            InflectionParams('subst', 'acc'), 2,
+            InflectionParams('ger', 'sg', 'nom', 'aff'), 1,
+            ('Pierwszą czynnością, jaką należy wykonać, jest ', ' przez ', '.')),
         SentenceDef(
             InflectionParams('subst', 'acc'), 2,
             InflectionParams('ger', 'sg', 'gen', 'aff'), 1,
